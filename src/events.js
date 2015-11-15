c16b5af32b457f755785712c6b4f41abc6d6dfa7
Rob Halff <rob.halff@gmail.com>
2015-11-15 16:50:53 +0100

    break up with mixins

---
+++ src/events.js
@@ -0,0 +1,137 @@
+import Emitter from 'wildemitter'
+
+export default class Events {
+  on(event, listener) {
+    super.on(event, listener)
+  }
+
+  off(event, listener) {
+    super.removeListener(event, listener)
+  }
+
+  /**
+   *
+   * Adds an event handler.
+   *
+   * It's possible to trigger multiple actions per path/event pair
+   *
+   * e.g.
+   *
+   *   dp.addEventHandler('click', '0:1:1', 'my-action')
+   *   dp.addEventHandler('click', 'move-up', 'my-action')
+   *
+   * @param {String} type The type
+   * @param {String} alias The path/alias
+   * @param {String} action action name
+   * @returns {undefined} Undefined
+   */
+  addEventHandler(type, alias, action) {
+    const path = this.dp.dealias(alias)
+    if (!this.handlers.has(type)) {
+      this.handlers.set(type, new Map())
+      this.dp.on(type, this._handleEvent.bind(this))
+    }
+
+    if (!this.handlers.get(type).has(path)) {
+      this.handlers.get(type).set(path, [])
+    }
+
+    const actions = this.handlers.get(type).get(path)
+    if (actions.indexOf(action) === -1) {
+      actions.push(action)
+    } else {
+      throw Error(`Duplicate action "${action}" for path ${path}`)
+    }
+  }
+
+  /**
+   *
+   * Remove event handler
+   *
+   * @param {String} type The type
+   * @param {String} alias The path/alias
+   * @param {String} action action name
+   * @returns {undefined} Undefined
+   */
+  removeEventHandler(type, alias, action) {
+    const path = this.dp.dealias(alias)
+    if (this.handlers.has(type)) {
+      if (this.handlers.get(type).has(path)) {
+        const actions = this.handlers.get(type).get(path)
+        const index = actions.indexOf(action)
+        if (index >= 0) {
+          actions.splice(index, 1)
+          if (actions.length === 0) {
+            this.handlers.get(type).delete(path)
+            if (this.handlers.get(type).size === 0) {
+              this.handlers.delete(type)
+              this.dp.off(type)
+            }
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   *
+   * Remove all event handlers.
+   *
+   * If type is given will only remove those event types
+   *
+   * @param {String[]} type Event Type
+   * @returns {undefined} Undefined
+   */
+  removeEventHandlers(type = []) {
+    const types = Array.isArray(type) ? type : [type]
+    for (const key of this.handlers.keys()) {
+      if (!types.length || types.indexOf(key) >= 0) {
+        this.dp.off(key)
+        this.handlers.delete(key)
+      }
+    }
+  }
+
+  /**
+   *
+   * Takes a global event and test whether this path
+   * or one of it's parents is listening for the event.
+   *
+   * @param {DomEvent} ev Event listener on window
+   * @returns {undefined} Undefined
+   */
+  _handleEvent(ev) {
+    // TODO: ev.preventDefault()
+    ev.stopPropagation()
+    if (!this.handlers.has(ev.type)) {
+      return
+    }
+
+    const cp = this.dp.path(ev.srcElement).split(':')
+    while (cp.length - 1) {
+      let epath = cp.join(':')
+      const el = this.dp.getRef(epath)
+
+      // lookup original if there was no specific event specified for this row.
+      // TODO: should it always also run an event handler defined for the source?
+      if (!this.handlers.get(ev.type).has(epath)) {
+        if (el.dataset && el.dataset.teflonOwner && el.dataset.teflonOwner !== epath) {
+          // match as owner, emit as ourselves
+          epath = el.dataset.teflonOwner
+        }
+      }
+
+      if (this.handlers.get(ev.type).has(epath)) {
+        const actions = this.handlers.get(ev.type).get(epath)
+        for (const action of actions) {
+          this.emit(action, ev, el)
+        }
+        // TODO: only break if stopPropagation
+        break
+      }
+      cp.pop()
+    }
+  }
+}
+
+Emitter.mixin(Events)
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import DomPointer from 'dompointer'
-import Emitter from 'wildemitter'
 import Dot from 'dot-object'
-import State from './state'
 import { mixin } from './util'
 import Repeat from './data/repeat'
+import States from './states'
+import Events from './events'
 
 export default class Teflon {
   constructor(dp) {
@@ -95,14 +95,6 @@ export default class Teflon {
     })
   }
 
-  on(event, listener) {
-    super.on(event, listener)
-  }
-
-  off(event, listener) {
-    super.removeListener(event, listener)
-  }
-
   /**
    *
    * Renders the current node.
@@ -116,78 +108,6 @@ export default class Teflon {
   render() {
     this.dp.render()
     return this
-  }
-
-  /**
-   *
-   * Takes a global event and test whether this path
-   * or one of it's parents is listening for the event.
-   *
-   * @param {DomEvent} ev Event listener on window
-   * @returns {undefined} Undefined
-   */
-  _handleEvent(ev) {
-    // TODO: ev.preventDefault()
-    ev.stopPropagation()
-    if (!this.handlers.has(ev.type)) {
-      return
-    }
-
-    const cp = this.dp.path(ev.srcElement).split(':')
-    while (cp.length) {
-      let epath = cp.join(':')
-      const el = this.dp.getRef(epath)
-
-      if (el.dataset && el.dataset.teflonOwner && el.dataset.teflonOwner !== epath) {
-        // match as owner, emit as ourselves
-        epath = el.dataset.teflonOwner
-      }
-
-      if (this.handlers.get(ev.type).has(epath)) {
-        const actions = this.handlers.get(ev.type).get(epath)
-        for (const action of actions) {
-          this.emit(action, ev, el)
-        }
-        // TODO: only break if stopPropagation
-        break
-      }
-      cp.pop()
-    }
-  }
-
-  /**
-   *
-   * Adds an event handler.
-   *
-   * It's possible to trigger multiple actions per path/event pair
-   *
-   * e.g.
-   *
-   *   dp.addEventHandler('click', '0:1:1', 'my-action')
-   *   dp.addEventHandler('click', 'move-up', 'my-action')
-   *
-   * @param {String} type The type
-   * @param {String} alias The path/alias
-   * @param {String} action action name
-   * @returns {undefined} Undefined
-   */
-  addEventHandler(type, alias, action) {
-    const path = this.dp.dealias(alias)
-    if (!this.handlers.has(type)) {
-      this.handlers.set(type, new Map())
-      this.dp.on(type, this._handleEvent.bind(this))
-    }
-
-    if (!this.handlers.get(type).has(path)) {
-      this.handlers.get(type).set(path, [])
-    }
-
-    const actions = this.handlers.get(type).get(path)
-    if (actions.indexOf(action) === -1) {
-      actions.push(action)
-    } else {
-      throw Error(`Duplicate action "${action}" for path ${path}`)
-    }
   }
 
   /**
@@ -221,54 +141,6 @@ export default class Teflon {
    */
   getElement() {
     return this.dp.el
-  }
-
-  /**
-   *
-   * Remove event handler
-   *
-   * @param {String} type The type
-   * @param {String} alias The path/alias
-   * @param {String} action action name
-   * @returns {undefined} Undefined
-   */
-  removeEventHandler(type, alias, action) {
-    const path = this.dp.dealias(alias)
-    if (this.handlers.has(type)) {
-      if (this.handlers.get(type).has(path)) {
-        const actions = this.handlers.get(type).get(path)
-        const index = actions.indexOf(action)
-        if (index >= 0) {
-          actions.splice(index, 1)
-          if (actions.length === 0) {
-            this.handlers.get(type).delete(path)
-            if (this.handlers.get(type).size === 0) {
-              this.handlers.delete(type)
-              this.dp.off(type)
-            }
-          }
-        }
-      }
-    }
-  }
-
-  /**
-   *
-   * Remove all event handlers.
-   *
-   * If type is given will only remove those event types
-   *
-   * @param {String[]} type Event Type
-   * @returns {undefined} Undefined
-   */
-  removeEventHandlers(type = []) {
-    const types = Array.isArray(type) ? type : [type]
-    for (const key of this.handlers.keys()) {
-      if (!types.length || types.indexOf(key) >= 0) {
-        this.dp.off(key)
-        this.handlers.delete(key)
-      }
-    }
   }
 
   /**
@@ -330,179 +202,8 @@ export default class Teflon {
     return this
   }
 
-  /**
-   *
-   * Has state
-   *
-   * @param {String} name State name to test
-   * @param {String} path Instance path
-   * @returns {Boolean} Whether this state exists
-   */
-  hasState(name, path) {
-    if (this.state.has(name)) {
-      if (path) {
-        const state = this.state.get(name)
-        return state.constructor.name === 'Map' && state.has(path)
-      }
-      return true
-    }
-    return false
-  }
-
-  /**
-   *
-   * Get state
-   *
-   * @param {String} name State name to test
-   * @param {String} path Instance path
-   * @returns {Boolean} Whether this state exists
-   */
-  getState(name, path) {
-    if (this.hasState(name, path)) {
-      if (path) {
-        return this.state.get(name).get(path)
-      }
-      return this.state.get(name)
-    }
-    throw Error(`State ${name} does not exist`)
-  }
-
-  /**
-   *
-   * Whether this state is activated
-   *
-   * @param {String} name State name to test
-   * @param {String} path Instance path
-   * @returns {Boolean} Whether this state is activated
-   */
-  inState(name, path) {
-    return this.getState(name, path).isActive
-  }
-
-  toggleState(name, path, clear) {
-    if (!this.hasState(name, path) || !this.inState(name, path)) {
-      if (clear) {
-        // BIG HACK, it includes the init.
-        this.activateState(name, path)
-        this.disableAll(name)
-      }
-      this.activateState(name, path)
-    } else {
-      this.disableState(name, path)
-    }
-  }
-
-  /**
-   *
-   * Activates a predefined state
-   *
-   * @param {String} name State name to activate
-   * @param {String} path Instance path
-   * @returns {Teflon} this instance
-   */
-  activateState(name, path) {
-    if (path && !this.hasState(name, path)) {
-      if (!this._prototype[name]) {
-        this._prototype[name] = this.getState(name)
-        this.state.set(name, new Map())
-      }
-      this.getState(name).set(path, this._prototype[name].clone(path))
-    }
-    if (this.inState(name, path)) {
-      const fname = path ? `${name}[${path.split(':').pop()}]` : name
-      throw Error(`State ${fname} already activated`)
-    }
-    this.getState(name, path).activate()
-    return this
-  }
-
-  /**
-   *
-   * disables a predefined state
-   *
-   * @param {String} name State name to disable
-   * @param {String} path Instance path
-   * @returns {Teflon} this instance
-   */
-  disableState(name, path) {
-    if (!this.inState(name, path)) {
-      throw Error(`State ${name} is already disabled`)
-    }
-    this.getState(name, path).disable()
-    return this
-  }
-
-  disableAll(name) {
-    const state = this.state.get(name)
-    if (state.constructor.name === 'Map') {
-      for (const path of state.keys()) {
-        if (this.inState(name, path)) {
-          this.disableState(name, path)
-        }
-      }
-    } else {
-      throw Error('disableAll can only be used for instances')
-    }
-  }
-
-  /**
-   *
-   * Adds a new state in addition of the defaultState.
-   *
-   * A state consists of attribute changes and/or event handler changes.
-   *
-   * @param {String} name State name
-   * @param {Map} state States Definition
-   * @returns {Teflon} this instance
-   */
-  addState(name, state) {
-    if (!this.state.has(name)) {
-      this.state.set(name, new State(name, state, this))
-      return this
-    }
-    throw Error(`State "${name}" already added`)
-  }
-
-  /**
-   *
-   * Removes a state
-   *
-   * @param {String} name State name
-   * @param {String} path Instance path
-   * @returns {Teflon} this instance
-   */
-  removeState(name, path) {
-    if (this.hasState(name, path)) {
-      this.disableState(name, path)
-      if (path) {
-        this.state.get(name).delete(path)
-      } else {
-        this.state.delete(name)
-      }
-      return this
-    }
-    throw Error(`State "${name}" does not exist`)
-  }
-
-  /**
-   *
-   * Set the state map for this template
-   *
-   * @param {Object} map State map
-   * @param {Object} map.events State Events
-   * @param {Object} map.attributes State Attributes
-   * @returns {Teflon} this instance
-   */
-  setStateMap(map) {
-    Object.keys(map).forEach((name) => {
-      this.addState(name, map[name])
-    })
-    if (this.hasState('default')) {
-      this.activateState('default')
-    }
-    return this
-  }
 }
 
-Emitter.mixin(Teflon)
+mixin(Teflon, Events)
 mixin(Teflon, Repeat)
+mixin(Teflon, States)
